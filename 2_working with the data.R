fe66439354ee73de0a4130dbe8cdf03b4e90e9a0
bwcastillo <bwcastillo@uc.cl>
2023-02-01 22:16:45 -0500

    code ordered first windows 5 M

--- 2_working with the data.R
+++ 2_working with the data.R
@@ -38,29 +38,16 @@ shootings <- st_read(conn, "nypd_shooting_historic") %>%
   st_set_crs(.,4326) 
 
 
-#Apparently, conceptually and according the available daata, would be easier analyze shooting
-#It would possible to analyze arrest but require more time maybe to apply manually the formulas
-#or to find ways to do easier the process
 
-
-#See the administratives and statistical scales load data
-
-#Roger bivand book
-#-Observation window - density
-#-Marked point patterns, points on linear networks
-
-
-#Antonio Paez book
 
 ggplot(shootings[!is.na(shootings$perp_race),], aes(color=perp_race))+
   geom_sf()
 
-#Loading borough
+#Loading borough at PostGIS
 st_write(st_read("https://data.cityofnewyork.us/resource/7t3b-ywvw.geojson"),dsn = conn, 'borough_nyc')
 
-unique(shootings$perp_age_group)
 
-
+#Querying intersects as table
 test <- dbSendQuery(conn,"SELECT ST_Intersects(nypd_shooting_historic.geometry, borough_nyc.geometry)
             FROM nypd_shooting_historic, borough_nyc;")
 
@@ -68,10 +55,47 @@ test <- dbFetch(test)
 
 #OR
 
+#Querying intersects as shapefile
 test <- st_read(conn,query="SELECT ST_Intersects(nypd_shooting_historic.geometry, borough_nyc.geometry)
                 FROM nypd_shooting_historic, borough_nyc;")
 
+# It returns tables without geometries 
 test<- st_read(conn,query="SELECT boro_name, perp_sex, vic_sex, occur_date, occur_time, perp_age_group, 
-                           vic_age_group, boroFROM borough_nyc, nypd_shooting_historic
+                           vic_age_group FROM borough_nyc, nypd_shooting_historic
                     WHERE ST_Intersects(nypd_shooting_historic.geometry, borough_nyc.geometry)")
 
+
+
+
+
+# Observation Window ------------------------------------------------------
+
+w1 <- st_bbox(shootings)|>st_as_sfc() #Create a Polygon based in the bbox
+
+w2 <- st_point(c(st_bbox(shootings)[3], ((st_bbox(shootings)[2]+st_bbox(shootings)[4]))/2))|>st_buffer(st_bbox(shootings)[3]-st_bbox(shootings)[1])
+
+#Windows 1
+plot(w1)
+plot(shootings,add=T)
+
+#Windows 2
+plot(w2)
+plot(shootings,add=T)
+
+#Windows arrest from PostGIS 
+plot(w1)
+plot(st_read(dsn=conn,layer="nypd_arrest_historic")
+     ,add=T) #It works but is hyper memory consumer
+gc()
+
+plot(w2)
+plot(read_sf(dsn=conn,layer="nypd_arrest_historic")# In this case works better to do closer windows
+     ,add=T) #It works but is hyper memory consumer
+
+
+install.packages("rgdal")
+
+test <- st_read(dsn=conn,layer="nypd_shooting_historic")
+class(test)
+
+test <- rgdal::readOGR(dsn=conn,layer="nypd_arrest_historic")
